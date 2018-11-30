6ec460d1639afa1a7aa54a089c2d3faf78bf940b
DercilioFontes <derciliofontes@gmail.com>
2018-11-29 19:59:39 -0800

    Codewar practices.

--- Product of consecutive Fib numbers/productFib.java
+++ Product of consecutive Fib numbers/productFib.java
@@ -2,16 +2,16 @@ public class ProdFib { // must be public for codewars
 	
 	public static long[] productFib(long prod) {
     
-    long numAnt = 0;
-    long numPost = 1;
+    long numAnt = 0L;
+    long numPost = 1L;
     
     while(prod > numAnt * numPost) {
       numPost = numPost + numAnt;
       numAnt = numPost - numAnt;
     }
     
-    long[] result = { numAnt, numPost, ( prod == numAnt * numPost) ? 1 : 0 };
+    long[] result = { numAnt, numPost, prod == numAnt * numPost ? 1 : 0 };
     
-		return result;
+	return result;
    }
 }
